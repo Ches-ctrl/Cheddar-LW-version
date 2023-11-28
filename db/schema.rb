1f38b46f72ccb02b61764faaccfc9dfd7be911cb
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-28 11:24:18 +0000

    added fields to user devise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_070217) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_112342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,17 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_070217) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "linkedin_profile"
+    t.string "address_first"
+    t.string "address_second"
+    t.string "post_code"
+    t.string "city"
+    t.string "phone_number"
+    t.string "github_profile_url"
+    t.string "website_url"
+    t.string "cover_letter_template_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
