6d1b13f18a429eb5f34ab6f51f659cc18460a487
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-28 13:04:12 +0000

    seeded some companies and jobs to test jobs index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :companies, only: [:index, :show]
   resources :jobs, only: [:index, :show]
 end
