48f915c2184b3982fae6f0e0d2d44df8969f6337
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-28 12:20:41 +0000

    added company model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_112342) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_122009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "company_name"
+    t.string "company_website_url"
+    t.string "company_category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
