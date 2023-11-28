99460ffd4f56c85cc38000beb958f2594a218193
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-28 12:27:02 +0000

    added job model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_122009) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_122437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,20 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_122009) do
     t.string "company_category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "job_title"
+    t.string "job_description"
+    t.integer "salary"
+    t.date "date_created"
+    t.text "application_criteria"
+    t.date "application_deadline"
+    t.string "job_posting_url"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +59,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_122009) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jobs", "companies"
 end
