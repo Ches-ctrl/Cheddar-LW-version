57dc876e54f15fee804a9d2f4088a10a41319069
Ilya Obretetskiy <obreil54@gmail.com>
2023-11-28 16:04:47 +0000

    Intial form filler setup done

--- app/services/form_filler.rb
+++ app/services/form_filler.rb
@@ -1,35 +1,34 @@
 require 'capybara/rails'
-# require 'capybara/dsl'
+require 'capybara/dsl'
 
 class FormFiller
-  # include Capybara::DSL
+  include Capybara::DSL
 
   def initialize
     Capybara.run_server = false
-    Capybara.register_driver :headless_chrome do |app|
+    Capybara.register_driver :selenium do |app|
       options = Selenium::WebDriver::Chrome::Options.new
       # options.add_argument("--headless")
       options.add_argument("--no-sandbox")
       options.add_argument("--window-size=1400,1000")
-      options.add_argument("--disable-gpu")
+      # options.add_argument("--disable-gpu")
       options.add_argument("--disable-dev-shm-usage")
       options.add_argument("--remote-debugging-port=9225")
       Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
     end
+
+    Capybara.default_driver = :selenium
   end
 
   def fill_out_form
-    session = Capybara::Session.new(:headless_chrome)
     puts "Before Page"
-    p session
-    p session.methods.sort
-    session.visit('https://www.example.com')
-    # session.fill_in(‘Username’, with: ‘your_username’)
-    # session.visit("https://apply.workable.com/benevolentai/j/28334DF927/apply/")
+    visit("https://apply.workable.com/benevolentai/j/28334DF927/apply/")
+    # fill_in(‘Username’, with: ‘your_username’)
     puts "After Page"
     puts page.html
-    session.fill_in('firstname', with: 'Ilya')
-    session.click_button "Submit application"
+    fill_in('firstname', with: 'Ilya')
+    fill_in('lastname', with: 'Obretetskiy')
+    click_button "Submit application"
   end
 end
 
