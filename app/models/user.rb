5c5122080e7d3f58a54ba178fd9e918e196ddfa7
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-28 16:26:55 +0000

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :address_first, :address_second, :post_code, :city, presence: true
   validates :email, uniqueness: true
   validates :password, presence: true, on: :create
+
+  has_many :job_applications, dependent: :destroy
+  has_many :jobs, through: :job_applications
 end
