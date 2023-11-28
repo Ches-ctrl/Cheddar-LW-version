ae2d1856f21c46540cc6070489f770b8b17d8600
Charlie Cheesman <djnr.cheesman@gmail.com>
2023-11-28 16:10:00 +0000

    adding styling to navbar, footer, index and home pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,47 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+puts "Deleting previous users, jobs and companies..."
+
+User.destroy_all
+Job.destroy_all
 Company.destroy_all
 
-5.times do
-  Company.create!(company_name: Faker::Company.name, company_category: Faker::Company.industry)
-end
+puts "Creating new jobs and companies..."
 
-Job.destroy_all
+5.times do
+  Company.create!(company_name: Faker::Company.name,
+                  company_category: Faker::Company.industry)
+  puts "Created company - #{Company.last.company_name}"
+end
 
 10.times do
-  Job.create!(job_title: Faker::Job.title, job_description: Faker::Job.field,
+  Job.create!(job_title: Faker::Job.title,
+              job_description: Faker::Job.field,
               salary: Faker::Number.number(digits: 5),
               company_id: Company.all.sample.id)
+  puts "Created job - #{Job.last.job_title}"
 end
+
+puts "Creating 4 users..."
+
+User.create(email: "email1@gmail.com", password: "password", first_name: "Charlotte", last_name: "the genius", address_first: "Le Wagon", address_second: "London", post_code: "E1 6JJ", city: "London")
+puts "Created user:  #{User.first.first_name}"
+User.create(email: "email2@gmail.com", password: "password", first_name: "Ilya", last_name: "the russian hacker", address_first: "Le Wagon", address_second: "London", post_code: "E1 6JJ", city: "London")
+puts "Created user:  #{User.second.first_name}"
+User.create(email: "email3@gmail.com", password: "password", first_name: "Direncan", last_name: "the mysterious", address_first: "Le Wagon", address_second: "London", post_code: "E1 6JJ", city: "London")
+puts "Created user:  #{User.third.first_name}"
+User.create(email: "email4@gmail.com", password: "password", first_name: "Charlie", last_name: "mr robot", address_first: "Le Wagon", address_second: "London", post_code: "E1 6JJ", city: "London")
+puts "Created user:  #{User.fourth.first_name}"
+
+# TODO: Seed applications
+# TODO: Connect Users to Applications
+
+
+puts "Created 4 Super Users"
+
+puts Company.all
+puts Job.all
+puts User.all
+
+puts "Done!"
