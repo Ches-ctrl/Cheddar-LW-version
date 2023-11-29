360507a7d5909d5392fa42de088d55bb1896c3f6
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-29 18:00:48 +0000

    added education table and controller

---
+++ app/controllers/educations_controller.rb
@@ -0,0 +1,15 @@
+class EducationsController < ApplicationController
+  def new
+    @education = Education.new
+  end
+
+  def create
+    @education = Education.new(education_params)
+    @education.user = current_user
+    if @education.save
+      redirect_to profile_path(current_user), notice: 'Education was successfully added'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
