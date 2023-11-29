e4046e8399bb1e875eda13b66efdb7cce7cba3c8
Charlie Cheesman <djnr.cheesman@gmail.com>
2023-11-29 18:05:41 +0000

    adding social proof and updating icons

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "about"  => "pages#about"
+  get "test" => "pages#test"
   # Defines the root path route ("/")
   # root "posts#index"
 
