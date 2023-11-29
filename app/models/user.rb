ac2f1d8cc9fd8813fd04918f00d87afa0026bf37
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-29 11:42:56 +0000

    created the job_applications index view and put link in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, :address_first, :post_code, :city, presence: true
+  validates :first_name, :last_name, presence: true
   validates :email, uniqueness: true
   validates :password, presence: true, on: :create
 
