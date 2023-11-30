f76b39aa2f8de02853edbca2d1ebc9e78cc794ea
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-30 16:30:11 +0000

    search by title

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,11 @@ class JobsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @jobs = Job.all
+    if params[:query].present?
+      @jobs = Job.search_all_strings(params[:query])
+    else
+      @jobs = Job.all
+    end
     @job = Job.new
   end
 
--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,12 @@ class Job < ApplicationRecord
   has_many :saved_jobs, dependent: :destroy
 
   validates :job_title, :job_description, :application_deadline, presence: true
+
+  include PgSearch::Model
+
+  pg_search_scope :search_all_strings,
+    against: [:job_title, :job_description, :application_criteria],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
