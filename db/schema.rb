0e53de297466759541fc9e4df91e0362007963d6
Ilya Obretetskiy <obreil54@gmail.com>
2023-11-30 17:40:10 +0000

    Updated version of the form logic to be integrated with Charlie's work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_125057) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_145015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,7 +71,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_125057) do
     t.bigint "company_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "saved", default: false, null: false
     t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
