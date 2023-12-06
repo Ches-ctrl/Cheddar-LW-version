02b19c8d864b53babccab0e8e4fb52925c616f6b
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-12-06 18:10:09 +0000

    combined filters

--- app/javascript/controllers/select_by_job_type_controller.js
+++ app/javascript/controllers/select_by_job_type_controller.js
@@ -2,18 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="select-by-job-type"
 export default class extends Controller {
-  static targets = [ 'type', 'jobRow' ]
+  static targets = [ 'role', 'jobRow', 'company' ]
 
   connect() {
   }
 
   searchByJobType() {
-    const checkedTypes = this.typeTargets.filter(type => type.checked)
-      .map(type => type.id)
+    const checkedRoles = this.roleTargets.filter(role => role.checked)
+      .map(role => role.id)
 
     const patternStrings = []
 
-    checkedTypes.forEach((query) => {
+    checkedRoles.forEach((query) => {
       const regexd_query = '(' + query.replace(' ', '(-| )?') + ')'
       // console.log(regexd_query)
       patternStrings.push(regexd_query)
@@ -30,6 +30,64 @@ export default class extends Controller {
       console.log(pattern.test(jobTitle))
       if (pattern.test(jobTitle)) {
         jobRow.classList.remove('d-none')
+      } else {
+        jobRow.classList.add('d-none')
+      }
+    })
+  }
+
+  searchByCompany() {
+    const checkedCompanies = this.companyTargets.filter(company => company.checked)
+      .map(company => company.attributes.id.value)
+
+    this.companyRowTargets.forEach((companyRow) => {
+      if (checkedCompanies.includes(companyRow.querySelector(".companyname").dataset.name)) {
+        companyRow.classList.remove('d-none')
+      } else {
+        companyRow.classList.add('d-none')
+      }
+      // companyRow.classList.toggle('d-none', !checkedCompanies.includes(companyRow.querySelector(".companyName").innerText))
+    })
+
+    if (checkedCompanies.length == 0) {
+      this.companyRowTargets.forEach((companyRow) => {
+        companyRow.classList.remove('d-none')})
+    }
+  }
+
+  combinedSearch() {
+    const checkedRoles = this.roleTargets.filter(role => role.checked)
+      .map(role => role.id)
+      console.log(checkedRoles);
+    const checkedCompanies = this.companyTargets.filter(company => company.checked)
+    .map(company => company.attributes.id.value)
+
+    const patternStrings = []
+
+    checkedRoles.forEach((query) => {
+      const regexd_query = '(' + query.replace(' ', '(-| )?') + ')'
+      // console.log(regexd_query)
+      patternStrings.push(regexd_query)
+    })
+
+    const pattern = new RegExp(patternStrings.join('|'), 'i');
+
+    this.jobRowTargets.forEach((jobRow) => {
+      const jobTitle = jobRow.querySelector(".role").dataset.role;
+      // console.log(jobRow.querySelector(".role"))
+      // console.log(jobRow.querySelector(".role").dataset)
+      // console.log(pattern);
+      console.log(checkedCompanies);
+      if (pattern.test(jobTitle)) {
+        if (checkedCompanies.length > 0) {
+          if (checkedCompanies.includes(jobRow.querySelector(".companyname").dataset.name)) {
+            jobRow.classList.remove('d-none')
+          } else {
+            jobRow.classList.add('d-none')
+          }
+        } else {
+          jobRow.classList.remove('d-none')
+        }
       } else {
         jobRow.classList.add('d-none')
       }
