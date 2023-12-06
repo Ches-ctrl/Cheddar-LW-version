8340f77bbf7fdc6390900e1c60811fd80aa762df
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-12-06 17:13:00 +0000

    updated job count on top left side of jobs index oage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,6 +14,7 @@ class JobsController < ApplicationController
     @job = Job.new
     @saved_job = SavedJob.new
     @saved_jobs = SavedJob.all
+    @job_applications = JobApplication.where(user_id: current_user.id)
   end
 
   def show
