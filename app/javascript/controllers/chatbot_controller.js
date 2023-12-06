d12a471d9493fce1530605da3b85905444a38e71
Charlie Cheesman <djnr.cheesman@gmail.com>
2023-12-06 23:27:32 +0000

    adding cheddargpt

--- app/javascript/controllers/chatbot_controller.js
+++ app/javascript/controllers/chatbot_controller.js
@@ -2,6 +2,78 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="chatbot"
 export default class extends Controller {
+  static targets = ["input", "messages", "modal"]
+
   connect() {
+    console.log("Hello, Stimulus!", this.element)
+  }
+
+  open() {
+    console.log("Open modal")
+    this.modalTarget.classList.add("show");
+    this.modalTarget.style.display = "block";
+    this.initializeOpenAIClient();
+  }
+
+  close() {
+    console.log("Close modal")
+    this.modalTarget.classList.remove("show");
+    this.modalTarget.style.display = "none";
+  }
+
+  initializeOpenAIClient() {
+    // Code to initialize or flag the OpenAI client
+    console.log("Initializing OpenAI client");
+    // Actual initialization should be done server-side
+  }
+
+  chat() {
+    const message = this.inputTarget.value.trim();
+    if (message === "") return;
+    fetch('/chatbot/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': document.querySelector("[name='csrf-token']").content
+      },
+      body: JSON.stringify({ content: message })
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response
+      this.appendMessage("User", message); // Append user's message
+      if (data.chatbotResponse) {
+        this.appendMessage("CheddarGPT", data.chatbotResponse); // Append chatbot's response
+        console.log(data.chatbotResponse)
+      }
+    });
+
+    this.inputTarget.value = "";
+  }
+
+  appendMessage(sender, text) {
+    console.log(sender)
+    console.log(text)
+
+    let messageElement = document.createElement("div");
+    console.log(messageElement)
+
+    messageElement.innerHTML = `<p>${text}</p>`;
+
+    messageElement.classList.add("message-card", sender === "User" ? "self" : "");
+
+    // messageElement.classList.add("message-card", sender === "User" ? "self" : "");
+
+
+    console.log(messageElement)
+    messageElement.innerHTML = `<p>${text}</p>`;
+    console.log(messageElement)
+
+    // Assuming 'messagesTarget' is the container for chat messages
+    this.messagesTarget.appendChild(messageElement);
+    console.log(this.messagesTarget)
+
+    // Scroll to the latest message
+    this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
   }
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_06_080345) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_06_212237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -128,6 +128,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_06_080345) do
     t.text "application_process"
     t.boolean "captcha"
     t.index ["company_id"], name: "index_jobs_on_company_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.boolean "self"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
