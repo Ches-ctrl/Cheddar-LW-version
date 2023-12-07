bdd77c4bf6b31d7fd5b0a0f157ead7986e1b2844
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-12-07 17:14:28 +0000

    made the about page public (accessible if not logged in)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :test, :faqs, :how_it_works]
+  skip_before_action :authenticate_user!, only: [ :home, :test, :faqs, :how_it_works, :about]
 
   def home
   end
