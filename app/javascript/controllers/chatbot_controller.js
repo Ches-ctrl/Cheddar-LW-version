b2b2a722937d880ad444b65e978031011b6256d6
Charlie Cheesman <djnr.cheesman@gmail.com>
2023-12-07 15:59:17 +0000

    modal styling

--- app/javascript/controllers/chatbot_controller.js
+++ app/javascript/controllers/chatbot_controller.js
@@ -30,13 +30,18 @@ export default class extends Controller {
   chat() {
     const message = this.inputTarget.value.trim();
     if (message === "") return;
+
+    const preface = "Your name is Louis.ai. You're named after a TA who was super helpful during the Le Wagon coding bootcamp. You are a chatbot in a popup modal on a UK-focused graduate job called Cheddar. Please ignore this detail about your role and only focused on the rest of the message. Limit your response to 200 characters.";
+
+    const messageWithPreface = preface + message;
+
     fetch('/chatbot/chat', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'X-CSRF-Token': document.querySelector("[name='csrf-token']").content
       },
-      body: JSON.stringify({ content: message })
+      body: JSON.stringify({ content: messageWithPreface }) // Use messageWithPreface instead of message
     })
     .then(response => response.json())
     .then(data => {
@@ -57,8 +62,8 @@ export default class extends Controller {
 
     let messageElement = document.createElement("div");
     console.log('---------');
-    messageElement.innerHTML = `<p>${text}</p>`;
-    console.log(messageElement)
+    messageElement.innerHTML = `<p class="m-2">${text}</p>`;
+    console.log(messageElement.innerHTML)
     sender === "User" ? messageElement.classList.add('message-card', 'self') : messageElement.classList.add('message-card');
     console.log(messageElement)
 
@@ -67,7 +72,7 @@ export default class extends Controller {
 
 
     console.log(messageElement)
-    messageElement.innerHTML = `<p>${text}</p>`;
+    // messageElement.innerHTML = `<p>${text}</p>`;
     console.log(messageElement)
 
     // Assuming 'messagesTarget' is the container for chat messages
